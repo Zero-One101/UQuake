a4de4a817adec0942e6018e907900a5da840ed8e
Stephen Badrick <stephenbadrick@gmail.com>
2017-02-10 20:25:36 +0000

    Projectiles now replicate

--- Source/UQuake/UQuakeProjectile.cpp
+++ Source/UQuake/UQuakeProjectile.cpp
@@ -30,6 +30,9 @@ AUQuakeProjectile::AUQuakeProjectile()
 
 	// Die after 3 seconds by default
 	InitialLifeSpan = 3.0f;
+
+    bReplicates = true;
+    bReplicateMovement = true;
 }
 
 void AUQuakeProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
